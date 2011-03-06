125c4aba6fb6c73935f3d976a0a9f76c291a186a
Carl Fürstenberg <azatoth@gmail.com>
2011-03-06 19:02:46 +0100

    adding new sync script written in perl
    this perl script allows you to easly download pages from wikipedia which
    are related to the files in here.

---
+++ sync.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl 
+
+use v5.10;
+
+use Git::Repository;
+use MediaWiki::Bot;
+use File::Slurp;
+use Getopt::Long::Descriptive;
+
+my $config_file = "$ENV{HOME}/.mwbotrc";
+my $c = {do "$config_file"} if -f $config_file;
+
+my ($opt, $usage) = describe_options(
+    "$0 %o <files...>",
+    [ 'username|u=s', "username for account on wikipedia", {default => $c->{username} // ""} ],
+    [ 'password|p=s', "password for account on wikipedia (do not use)", {default => $c->{password} // ""} ],
+    [ 'base|b=s', "base localtion on wikipedia where files exists (default User:AzaToth)", {default => "User:AzaToth"} ],
+    [ 'mode' => hidden =>
+        {
+            required => 1,
+            one_of => [
+                ["pull" => "pull changes from wikipedia"],
+                ["push" => "push changes to wikipedia"]
+            ] 
+        } 
+    ],
+    [],
+    [ 'verbose|v',  "print extra stuff"            ],
+    [ 'help',       "print usage message and exit" ],
+);
+
+print($usage->text), exit if $opt->help || !scalar(@ARGV);
+
+my %pages = map +("$opt->{base}/$_" => $_), @ARGV;
+
+# Config file should be an hash consisting of username and password keys
+
+my $repo = Git::Repository->new();
+
+my @status = $repo->run( status => '--porcelain');
+
+if( scalar @status ) {
+    say "repository is not clean. aborting...";
+    exit;
+}
+
+my $bot = MediaWiki::Bot->new({
+        assert      => 'user',
+        protocol    => 'http',
+        host        => 'en.wikipedia.org',
+        path        => 'w',
+        login_data  => { username => $opt->username, password => $opt->password},
+    }
+);
+
+if( $opt->mode eq "pull" ) {
+
+    while(my($page, $file) = each %pages) {
+        say "Grabbing $page";
+        my $text = $bot->get_text($page);
+        $text =~ s/\h+$//mg;
+        write_file( $file, {binmode => ':raw' }, $text);
+    }
+    my $cmd = $repo->command( diff => '--stat', '--color' );
+    my $s = $cmd->stdout;
+    while (<$s>) {
+        say $_;
+    }
+    $cmd->close;
+} elsif( $opt->mode eq "push" ) {
+    say "TODO: push is not implemented";
+}
