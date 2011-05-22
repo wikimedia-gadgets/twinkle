e5aa6446395e6b9d62f0137f18fa3d849fb63736
This, that and the other <at.light@live.com.au>
2011-05-22 11:17:45 +1000

    oldtwinkle: comment fix

--- oldtwinkle.js
+++ oldtwinkle.js
@@ -1,2 +1,4 @@
-// See [[MediaWiki:Gadget-Twinkle.js]]
+// Twinkle has moved!
+//  - see [[WP:TW]] for more information
+//  - for the code, see [[MediaWiki:Gadget-Twinkle.js]]
 mw.loader.load(['ext.gadget.Twinkle']);
