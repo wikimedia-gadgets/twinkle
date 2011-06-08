wikimedia-gadgets/twinkle

commit 550c8df9f777288088a12b2a23be9c52750ec9da

twinkleimage: add logging capability
At the moment this just calls into Twinkle.speedy.

modules/twinkleimage.js
@@ -202,13 +202,41 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 		old_image = event.target.old_image.checked;
 	}
 
+	var csdcrit;
+	switch( params.type ) {
+		case 'no source no license':
+		case 'no source':
+		case 'no license':
+			csdcrit = "F4";
+			break;
+		case 'orphaned fair use':
+			csdcrit = "F5";
+			break;
+		case 'no fair use rationale':
+			csdcrit = "F6";
+			break;
+		case 'disputed fair use rationale':
+		case 'replaceable fair use':
+			csdcrit = "F7";
+			break;
+		case 'no permission':
+			csdcrit = "F11";
+			break;
+		default:
+			throw "Twinkle.image.callback.evaluate: unknown criterion";
+	}
+
+	var lognomination = Twinkle.getPref('logSpeedyNominations') && Twinkle.getPref('noLogOnSpeedyNomination').indexOf(csdcrit) === -1;
+
 	var params = {
 		'type': type,
+		'normalized': csdcrit,
 		'non_free': non_free,
 		'source': source,
 		'reason': reason,
 		'replacement': replacement,
-		'old_image': old_image
+		'old_image': old_image,
+		'lognomination': lognomination
 	};
 	SimpleWindow.setButtonsEnabled( false );
 	Status.init( event.target );
@@ -225,7 +253,12 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 	if( notify ) {
 		wikipedia_page.lookupCreator(Twinkle.image.callbacks.userNotification);
 	} else {
-		// No auto-notifiaction, display what was going to be added.
+		// add to CSD log if desired
+		if (lognomination) {
+			params.fromDI = true;
+			Twinkle.speedy.callbacks.user.addToLog(params, null);
+		}
+		// No auto-notification, display what was going to be added.
 		var noteData = document.createElement( 'pre' );
 		noteData.appendChild( document.createTextNode( "{{subst:di-" + type + "-notice|1=" + mw.config.get('wgTitle') + "}} ~~~~" ) );
 		Status.info( 'Notification', [ 'Following/similar data should be posted to the original uploader:', document.createElement( 'br' ),  noteData ] );
@@ -238,42 +271,30 @@ Twinkle.image.callbacks = {
 		var params = pageobj.getCallbackParameters();
 
 		var tag = "{{di-" + params.type + "|date={{subst:#time:j F Y}}";
-		var csdcrit = "";
 		switch( params.type ) {
 			case 'no source no license':
 			case 'no source':
 				tag += params.non_free ? "|non-free=yes" : "";
-				csdcrit = "F4";
 				break;
 			case 'no permission':
 				tag += params.source ? "|source=" + params.source : "";
-				csdcrit = "F11";
 				break;
 			case 'disputed fair use rationale':
 				tag += params.reason ? "|concern=" + params.reason : "";
-				csdcrit = "F7";
 				break;
 			case 'orphaned fair use':
 				tag += params.replacement ? "|replacement=" + params.replacement : "";
-				csdcrit = "F5";
 				break;
 			case 'replaceable fair use':
 				tag += params.old_image ? "|old image=yes" : "";
-				csdcrit = "F7";
-				break;
-			case 'no license':
-				csdcrit = "F4";
-				break;
-			case 'no fair use rationale':
-				csdcrit = "F6";
 				break;
 			default:
-				break;
+				break;  // doesn't matter
 		}
 		tag += "}}\n";
 
 		pageobj.setPageText(tag + text);
-		pageobj.setEditSummary("This file is up for deletion, per [[WP:CSD#" + csdcrit + "|CSD " + csdcrit + "]] (" + params.type + ")." + Twinkle.getPref('summaryAd'));
+		pageobj.setEditSummary("This file is up for deletion, per [[WP:CSD#" + params.normalized + "|CSD " + params.normalized + "]] (" + params.type + ")." + Twinkle.getPref('summaryAd'));
 		switch (Twinkle.getPref('deliWatchPage')) {
 			case 'yes':
 				pageobj.setWatchlist(true);
@@ -309,5 +330,11 @@ Twinkle.image.callbacks = {
 		}
 		usertalkpage.setFollowRedirect(true);
 		usertalkpage.append();
+
+		// add this nomination to the user's userspace log, if the user has enabled it
+		if (params.lognomination) {
+			params.fromDI = true;
+			Twinkle.speedy.callbacks.user.addToLog(params, initialContrib);
+		}
 	}
 };

modules/twinklespeedy.js
@@ -1018,6 +1018,11 @@ Twinkle.speedy.callbacks = {
 			pageobj.save();
 		},
 
+		// note: this code is also invoked from twinkleimage
+		// the params used are:
+		//   for all: params.normalized
+		//   for CSD: params.value
+		//   for DI: params.fromDI = true, params.type
 		addToLog: function(params, initialContrib) {
 			var wikipedia_page = new Wikipedia.page("User:" + mw.config.get('wgUserName') + "/" + Twinkle.getPref('speedyLogPageName'), "Adding entry to userspace log");
 			params.logInitialContrib = initialContrib;
@@ -1048,25 +1053,30 @@ Twinkle.speedy.callbacks = {
 			}
 
 			text += "\n# [[:" + mw.config.get('wgPageName') + "]]: ";
-			switch (params.normalized)
-			{
-				case 'db':
-					text += "{{tl|db-reason}}";
-					break;
-				case 'multiple':
-					text += "multiple criteria (";
-					for (var i in Twinkle.speedy.dbmultipleCriteria) {
-						if (typeof Twinkle.speedy.dbmultipleCriteria[i] === 'string') {
-							text += '[[WP:CSD#' + Twinkle.speedy.dbmultipleCriteria[i].toUpperCase() + '|' + Twinkle.speedy.dbmultipleCriteria[i].toUpperCase() + ']], ';
+			if (params.fromDI) {
+				text += "DI [[WP:CSD#" + params.normalized.toUpperCase() + "|CSD " + params.normalized.toUpperCase() + "]] (" + params.type + ")";
+			} else {
+				switch (params.normalized)
+				{
+					case 'db':
+						text += "{{tl|db-reason}}";
+						break;
+					case 'multiple':
+						text += "multiple criteria (";
+						for (var i in Twinkle.speedy.dbmultipleCriteria) {
+							if (typeof Twinkle.speedy.dbmultipleCriteria[i] === 'string') {
+								text += '[[WP:CSD#' + Twinkle.speedy.dbmultipleCriteria[i].toUpperCase() + '|' + Twinkle.speedy.dbmultipleCriteria[i].toUpperCase() + ']], ';
+							}
 						}
-					}
-					text = text.substr(0, text.length - 2);  // remove trailing comma
-					text += ')';
-					break;
-				default:
-					text += "[[WP:CSD#" + params.normalized.toUpperCase() + "|CSD " + params.normalized.toUpperCase() + "]] ({{tl|db-" + params.value + "}})";
-					break;
+						text = text.substr(0, text.length - 2);  // remove trailing comma
+						text += ')';
+						break;
+					default:
+						text += "[[WP:CSD#" + params.normalized.toUpperCase() + "|CSD " + params.normalized.toUpperCase() + "]] ({{tl|db-" + params.value + "}})";
+						break;
+				}
 			}
+
 			if (params.logInitialContrib) {
 				text += "; notified {{user|" + params.logInitialContrib + "}}";
 			}

commit eef87b9e2db9308e09bfd591bf9f3eff71dffe96

twinkleimage: Fix issue #20, broken since 550c8df9f777288088a12b2a23be9c52750ec9da

modules/twinkleimage.js
@@ -203,7 +203,7 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 	}
 
 	var csdcrit;
-	switch( params.type ) {
+	switch( type ) {
 		case 'no source no license':
 		case 'no source':
 		case 'no license':