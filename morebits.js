520071098e5e6a1dea2f29c8c6eea2ef1c0d6280
This, that and the other <at.light@live.com.au>
2011-12-17 21:32:15 +1100

    morebits: remove Number.zeroFill - unused
    The ruthless modification of prototypes on primitive types (like Number and Array) has got us into trouble.
    See: https://bugzilla.wikimedia.org/33154
    So it's time to remove these and put them in separate objects (or, as in this case, remove them outright).

--- morebits.js
+++ morebits.js
@@ -2524,19 +2524,6 @@ Wikipedia.wiki.prototype = {
 
 
 /**
- * **************** Number ****************
- * REMOVEME - unused?
- */
-
-Number.prototype.zeroFill = function( length ) {
-	var str = this.toFixed();
-	if( !length ) { return str; }
-	while( str.length < length ) { str = '0' + str; }
-	return str;
-};
-
-
-/**
  * **************** MediaWiki ****************
  * Wikitext manipulation
  */
