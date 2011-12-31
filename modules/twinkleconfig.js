873657d674c2417c68f6319515d9fa5c71f84b2b
This, that and the other <at.light@live.com.au>
2011-12-31 11:00:16 +1100

    config: fix misdetection of two-letter usernames as ".js" subpages
    Logic error here, as User:Nx pointed out at WT:TW:
    > I have a username that's two characters long. mw.config.get("wgTitle").length - 3 returns (2 - 3 =) -1.
    > My userpage doesn't have .js in its title. mw.config.get("wgTitle").lastIndexOf(".js") returns -1.
    So I'm switching to wgPageName instead, which should fix this problem.

--- modules/twinkleconfig.js
+++ modules/twinkleconfig.js
@@ -1165,7 +1165,8 @@ Twinkle.config.init = function twinkleconfigInit() {
 			box.appendChild(document.createTextNode(", or by editing this page."));
 			$(box).insertAfter($("#contentSub"));
 
-		} else if (mw.config.get("wgTitle").indexOf(mw.config.get("wgUserName")) === 0 && mw.config.get("wgTitle").lastIndexOf(".js") == mw.config.get("wgTitle").length - 3) {
+		} else if (mw.config.get("wgTitle").indexOf(mw.config.get("wgUserName")) === 0 &&
+				mw.config.get("wgPageName").lastIndexOf(".js") == mw.config.get("wgPageName").length - 3) {
 			// place "Looking for Twinkle options?" notice
 			box.style.width = "60%";
 
