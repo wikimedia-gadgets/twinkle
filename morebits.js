0c8760465cf6869088373538c1cd956dec87db7a
This, that and the other <at.light@live.com.au>
2012-05-20 11:51:23 +1000

    morebits: fix Morebits.getPageAssociatedUser to work on non-English wikis
    Per issue #100.

--- morebits.js
+++ morebits.js
@@ -899,15 +899,14 @@ Morebits.array = {
  * Currently works on User:, User talk:, Special:Contributions.
  */
 Morebits.getPageAssociatedUser = function(){
-	var namespaceIds = mediaWiki.config.get("wgNamespaceIds");
 	var thisNamespaceId = mw.config.get('wgNamespaceNumber');
 
 	if ( thisNamespaceId === 2 /* User: */ || thisNamespaceId === 3 /* User talk: */ ) {
 		return mw.config.get('wgTitle').split( '/' )[0];  // only first part before any slashes, to work on subpages
 	}
 
-	if ( thisNamespaceId === namespaceIds['special'] && mw.config.get('wgCanonicalSpecialPageName') === "Contributions" ) {
-		return decodeURIComponent(/wiki\/Special:Log\/(.+)$/.exec($('div#contentSub a[title^="Special:Log"]').last().attr("href").replace(/_/g, "%20"))[1]);
+	if ( thisNamespaceId === -1 /* Special: */ && mw.config.get('wgCanonicalSpecialPageName') === "Contributions" ) {
+		return $('table.mw-contributions-table input[name="target"]')[0].getAttribute('value');
 	}
 
 	return false;
