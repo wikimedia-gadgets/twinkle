7b646f9221ab4eb108281113beacdb097f6b40d5
This, that and the other <at.light@live.com.au>
2013-01-24 11:22:37 +1100

    arv: fix typo in SPI sockmaster code

--- modules/twinklearv.js
+++ modules/twinklearv.js
@@ -457,7 +457,7 @@ Twinkle.arv.callback.evaluate = function(e) {
 			}
 
 			sockParameters.uid = puppetReport ? form.sockmaster.value.trimRight() : uid;
-			sockParameters.sockpuppets = puppetReport ? [uid] : $.map( $('input:text[@name=sockpuppet]',form), function(o){ return $(o).val(); });
+			sockParameters.sockpuppets = puppetReport ? [uid] : $.map( $('input:text[name=sockpuppet]',form), function(o){ return $(o).val(); });
 
 			Morebits.simpleWindow.setButtonsEnabled( false );
 			Morebits.status.init( form );
