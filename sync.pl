9a04c09a835761b5a7dbdfe49c4b94d107793267
Carl Fürstenberg <azatoth@gmail.com>
2013-02-03 15:04:36 +0100

    secure is now redirecting 301 back to normal

--- sync.pl
+++ sync.pl
@@ -52,8 +52,7 @@ my $repo = Git::Repository->new();
 my $bot = MediaWiki::Bot->new({
         assert      => 'user',
         protocol    => 'https',
-        host        => 'secure.wikimedia.org',
-        path        => "$opt->{family}/$opt->{lang}/w",
+        host        => 'en.wikipedia.org',
         login_data  => { username => $opt->username, password => $opt->password},
         debug => $opt->{verbose} ? 2 : 0,
 		maxlag => 1000 # not a botty script, thus smash it!
