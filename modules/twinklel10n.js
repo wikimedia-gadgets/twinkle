bf233bab430140f2f846a9b4b751d64cabc5aceb
Huji <huji.huji@gmail.com>
2016-03-14 00:43:45 +0000

    Switch is a better way

--- modules/twinklel10n.js
+++ modules/twinklel10n.js
@@ -17,18 +17,65 @@ TwinkleMessages.de = {
 };
 
 // Localizing twinkle is also easy, for example:
-if( mw.config.get('wgUserLanguage') == 'de' ){
-	Twinkle.shared.standardList = [
-		{
-			label: $.i18n._('tw-sharedip-shared-ip-label'),
-			value: $.i18n._('tw-sharedip-shared-ip'),
-			tooltip: $.i18n._('tw-sharedip-share-ip-tooltip')
-		},
-		{
-			label: $.i18n._('tw-sharedip-shared-ip-edu-label'),
-			value: $.i18n._('tw-sharedip-shared-ip-edu')
-		},
-	];
+switch( mw.config.get('wgUserLanguage') ) {
+	case 'en':
+		Twinkle.shared.standardList = [
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-label'),
+				value: $.i18n._('tw-sharedip-shared-ip'),
+				tooltip: $.i18n._('tw-sharedip-share-ip-tooltip')
+			},
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-edu-label'),
+				value: $.i18n._('tw-sharedip-shared-ip-edu')
+			},
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-corp-label'),
+				value: $.i18n._('tw-sharedip-shared-ip-corp')
+			},
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-public-label'),
+				value: $.i18n._('tw-sharedip-shared-ip-public')
+			},
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-gov-label'),
+				value: $.i18n._('tw-sharedip-shared-ip-gov')
+			},
+			{
+				label: $.i18n._('tw-sharedip-dynamic-ip-label'),
+				value: $.i18n._('tw-sharedip-dynamic-ip')
+			},
+			{
+				label: $.i18n._('tw-sharedip-static-ip-label'),
+				value: $.i18n._('tw-sharedip-static-ip')
+			},
+			{
+				label: $.i18n._('tw-sharedip-isp-label'),
+				value: $.i18n._('tw-sharedip-isp')
+			},
+			{
+				label: $.i18n._('tw-sharedip-mobile-ip-label'),
+				value: $.i18n._('tw-sharedip-mobile-ip')
+			},
+			{
+				label: $.i18n._('tw-sharedip-whois-label'),
+				value: $.i18n._('tw-sharedip-whois')
+			}
+		];
+	break;
+	case 'de':
+		Twinkle.shared.standardList = [
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-label'),
+				value: $.i18n._('tw-sharedip-shared-ip'),
+				tooltip: $.i18n._('tw-sharedip-share-ip-tooltip')
+			},
+			{
+				label: $.i18n._('tw-sharedip-shared-ip-edu-label'),
+				value: $.i18n._('tw-sharedip-shared-ip-edu')
+			},
+		];
+	break;
 };
 
 })(jQuery);
