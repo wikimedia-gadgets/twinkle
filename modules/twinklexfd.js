5fb9885223902d67df8f7d9b8f9b10a0b6bcd28e
Amory Meltzer <Amorymeltzer@gmail.com>
2019-01-21 12:42:23 -0500

    xfd and image: Remove check that prevented loading on history pages
    Closes #378
    
    As noted there, the check added in 8b65bb1 meant that twinkleimage would no
    longer load on history pages (similar to ad5af2f).  That seems to no longer be
    an issue, in particular as the structure and loading of Twinkle has changed
    dramatically since then.
    
    These changes are imperfect, however.  In order to allow loading on history
    pages, the conditions must be relaxed, such that it is possible for twinkle
    image and xfd to load on a local nonredirect page with no file on it.  One
    possible workaround could be a (morebits?) test for a file rather than trying
    to detect 'mw-imagepage-section-filehistory', but per
    [quarry:query/32775](https://quarry.wmflabs.org/query/32775) these seem rather
    rare (2-3 a month) so the benefit likely exceeds any perceived costs.

--- modules/twinklexfd.js
+++ modules/twinklexfd.js
@@ -18,8 +18,7 @@ Twinkle.xfd = function twinklexfd() {
 	// * special pages
 	// * non-existent pages
 	// * files on Commons, whether there is a local page or not (unneeded local pages of files on Commons are eligible for CSD F2)
-	// * file pages without actual files (these are eligible for CSD G8)
-	if ( mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId') || (mw.config.get('wgNamespaceNumber') === 6 && (document.getElementById('mw-sharedupload') || (!document.getElementById('mw-imagepage-section-filehistory') && !Morebits.wiki.isPageRedirect()))) ) {
+	if ( mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId') || (mw.config.get('wgNamespaceNumber') === 6 && (document.getElementById('mw-sharedupload'))) ) {
 		return;
 	}
 	Twinkle.addPortletLink( Twinkle.xfd.callback, "XFD", "tw-xfd", "Nominate for deletion" );
