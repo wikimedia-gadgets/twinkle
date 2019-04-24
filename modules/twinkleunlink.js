53bcb0cd1353691f3e2ed80bdd996f8d00e2692b
Siddharth VP <siddharthvp@gmail.com>
2019-04-25 00:45:18 +0530

    morebits: remove Morebits.wikipedia
    Based on the discussion at #103, specifically https://github.com/azatoth/twinkle/issues/103#issuecomment-477403897.
    
    On enwiki, Morebits.wikipedia.namespaces is just a copy of mw.config.get('wgFormattedNamespaces'). On other wikis, it is useless. mw.config.get('wgFormattedNamespaces') is better as it shows localised namespaces names across different wikis.
    
    Morebits.wikipedia.namespacesFriendly (also enwiki-only) is used just once in the codebase. This usage has been changed to use wgFormattedNamespaces.
    
    Also removed the 8-year-old comment noting the removal of Twinkle blacklist by someone, because it has nothing to do with morebits.

--- modules/twinkleunlink.js
+++ modules/twinkleunlink.js
@@ -164,7 +164,7 @@ Twinkle.unlink.callbacks = {
 					apiobj.params.form.append( { type:'header', label: 'File usage' } );
 					namespaces = [];
 					$.each(Twinkle.getPref('unlinkNamespaces'), function(k, v) {
-						namespaces.push(Morebits.wikipedia.namespacesFriendly[v]);
+						namespaces.push(v === 0 ? '(Article)' : mw.config.get('wgFormattedNamespaces')[v]);
 					});
 					apiobj.params.form.append( {
 						type: 'div',
@@ -210,7 +210,7 @@ Twinkle.unlink.callbacks = {
 				apiobj.params.form.append( { type:'header', label: 'Backlinks' } );
 				namespaces = [];
 				$.each(Twinkle.getPref('unlinkNamespaces'), function(k, v) {
-					namespaces.push(Morebits.wikipedia.namespacesFriendly[v]);
+					namespaces.push(v === 0 ? '(Article)' : mw.config.get('wgFormattedNamespaces')[v]);
 				});
 				apiobj.params.form.append( {
 					type: 'div',
