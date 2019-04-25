b5f69aba148596af606e6df030d20eec092fef43
Amory Meltzer <Amorymeltzer@gmail.com>
2019-04-25 09:49:17 +0530

    Update modules/twinkleunlink.js
    Co-Authored-By: siddharthvp <siddharthvp@gmail.com>

--- modules/twinkleunlink.js
+++ modules/twinkleunlink.js
@@ -164,7 +164,7 @@ Twinkle.unlink.callbacks = {
 					apiobj.params.form.append( { type:'header', label: 'File usage' } );
 					namespaces = [];
 					$.each(Twinkle.getPref('unlinkNamespaces'), function(k, v) {
-						namespaces.push(v === 0 ? '(Article)' : mw.config.get('wgFormattedNamespaces')[v]);
+						namespaces.push(v === '0' ? '(Article)' : mw.config.get('wgFormattedNamespaces')[v]);
 					});
 					apiobj.params.form.append( {
 						type: 'div',
