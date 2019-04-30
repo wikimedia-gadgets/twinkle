ed67db9af9956a3138cf7b8c9595e767d375019c
Amory Meltzer <Amorymeltzer@gmail.com>
2019-04-30 12:53:09 -0700

    Update modules/twinkleconfig.js
    Fix tab/spacing issue
    
    Co-Authored-By: DannyS712 <46829944+DannyS712@users.noreply.github.com>

--- modules/twinkleconfig.js
+++ modules/twinkleconfig.js
@@ -1689,7 +1689,7 @@ Twinkle.config.writePrefs = function twinkleconfigWritePrefs(pageobj) {
 		"// changing the configuration parameters in a valid-JavaScript way) will be\n" +
 		"// overwritten the next time you click \"save\" in the Twinkle preferences\n" +
 		"// panel.  If modifying this file, make sure to use correct JavaScript.\n" +
-	    	"// <no" + "wiki>\n"+
+		"// <no" + "wiki>\n" +
 		"\n" +
 		"window.Twinkle.prefs = ";
 	text += JSON.stringify(newConfig, null, 2);
