7c0f5bb9cd05e0536e29db775209307cc4ef7833
Amory Meltzer <Amorymeltzer@gmail.com>
2020-06-14 06:44:39 -0400

    morebits: Output API error code along with the error
    Unhelpful `null` if can't contact the API (no internet) but that's not a big deal.

--- morebits.js
+++ morebits.js
@@ -1834,7 +1834,7 @@ Morebits.wiki.api.prototype = {
 			});
 		}
 
-		this.statelem.error(this.errorText);
+		this.statelem.error(this.errorText + ' (' + this.errorCode + ')');
 
 		// invoke failure callback if one was supplied
 		if (this.onError) {
