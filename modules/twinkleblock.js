e6f7d3d99537118c82c4d2e3d17d30bd146a4add
wiki-ST47 <57046754+wiki-ST47@users.noreply.github.com>
2020-09-02 07:54:45 -0400

    Don't follow user talk page redirects when issuing block templates.
    This patch fixes security/harassment issue #912 by preventing following of redirects when issuing a block.

--- modules/twinkleblock.js
+++ modules/twinkleblock.js
@@ -1559,7 +1559,7 @@ Twinkle.block.callback.issue_template = function twinkleblockCallbackIssueTempla
 
 	var wikipedia_page = new Morebits.wiki.page(userTalkPage, 'User talk page modification');
 	wikipedia_page.setCallbackParameters(params);
-	wikipedia_page.setFollowRedirect(true, false);
+	wikipedia_page.setFollowRedirect(false);
 	wikipedia_page.load(Twinkle.block.callback.main);
 };
 
