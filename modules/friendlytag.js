cb47309d18c4aba084e374091b9fe31861d9c479
TheTVExpert <73130180+TheTVExpert@users.noreply.github.com>
2020-10-19 13:57:06 -0500

    Remove unneeded brackets

--- modules/friendlytag.js
+++ modules/friendlytag.js
@@ -918,14 +918,12 @@ Twinkle.tag.redirectList = {
 			{ 
 				tag: 'R avoided double redirect',
 				description: 'redirect from an alternative title for another redirect',
-				subgroup: [
-					{
-						name: 'doubleRedirectTarget',
-						type: 'input',
-						label: 'Redirect page name',
-						tooltip: 'Enter the page this redirect would target if the page wasn\'t also a redirect'
-					}
-				]
+				subgroup: {
+					name: 'doubleRedirectTarget',
+					type: 'input',
+					label: 'Redirect page name',
+					tooltip: 'Enter the page this redirect would target if the page wasn\'t also a redirect'
+				}
 			},
 			{ tag: 'R from file metadata link', description: 'redirect of a wikilink created from EXIF, XMP, or other information (i.e. the "metadata" section on some image description pages)' },
 			{ tag: 'R to list entry', description: 'redirect to a list which contains brief descriptions of subjects not notable enough to have separate articles' },
