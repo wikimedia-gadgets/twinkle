ca187d6615746c0325d9c74cc65ec03f75902fed
Siddharth VP <siddharthvp@gmail.com>
2020-12-02 03:38:09 +0530

    Set up use of TypeScript
    Added dev dependencies typescript and @types/jquery
    
    Created:
    - morebits.d.ts: type definitions for morebits.js
    - mediawiki.d.ts: type definitions for mw.* modules
    - jquery.d.ts.: wrapper around @types/jquery module for defining `$` and `jQuery`.
    
    `npm run build` can be used for compiling TS to JS with source maps (*.js.map files) which are ignored by git.

---
+++ types/mediawiki.d.ts
@@ -0,0 +1,206 @@
+/**
+ * Type definitions for mediawiki modules
+ */
+
+type title = string | mw.Title
+type namespaceId = number
+
+declare namespace mw {
+
+
+	class Api {
+		constructor(options?: any)
+		abort(): void
+		get(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+		post(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+
+		/**
+		 * @private
+		 */
+		preprocessParameters( parameters: any, useUS: boolean ): void
+
+		// index.js
+		ajax(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+		postWithToken(tokenType: string, params: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+		getToken( type: string, additionalParams?: any | string ): JQuery.Promise<string>
+		badToken(type: string): void
+		getErrorMessage(data: any): JQuery
+
+		// edit.js
+		postWithEditToken(params: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+		getEditToken(): JQuery.Promise<string>
+		create(title: title, params: any, content: string): JQuery.Promise<any>
+		edit(title: title, transform: (data: {
+			timestamp: string,
+			content: string
+		}) => any | string): JQuery.Promise<any>
+		newSection(title: title, header: string, message: string, additionalParams?: any): JQuery.Promise<any>
+
+		// user.js
+		getUserInfo(): JQuery.Promise<{
+			groups: string[],
+			rights: string[]
+		}>
+		assertCurrentUser(query: any): JQuery.Promise<{
+			assert: 'anon' | 'user'
+			assertUser: string
+		}>
+
+		// options.js
+		saveOption(name: string, value: string): JQuery.Promise<any>
+		saveOptions(options: {[optionName: string]: string}): JQuery.Promise<any>
+
+		// watch.js
+		watch(pages: title | title[]): JQuery.Promise<{
+			watch: {title: string, watched: boolean} | {title: string, watched: boolean}[]
+		}>
+		unwatch(pages: title | title[]): JQuery.Promise<{
+			watch: {title: string, watched: boolean} | {title: string, watched: boolean}[]
+		}>
+
+		// parse.js
+		parse(content: string | Title, additionalParams?: any): JQuery.Promise<any>
+
+		// messages.js
+		getMessages(messages: string[], options?: any): JQuery.Promise<any>
+		loadMessages(messages: string[], options?: any): JQuery.Promise<any>
+		loadMessagesIfMissing(messages: string[], options?: any): JQuery.Promise<any>
+
+
+		// TODO
+		// category.js
+		// login.js
+		// rollback.js
+		// upload.js
+
+	}
+
+	class Title {
+		constructor(title: string, namespace?: namespaceId)
+		static newFromText(title: string, namespace?: namespaceId): mw.Title | null
+		static makeTitle(title: string, namespace?: namespaceId): mw.Title | null
+		static newFromUserInput(title: string, namespace?: namespaceId, options?: any): mw.Title
+		static newFromFileName(uncleanName: string): mw.Title
+		static newFromImg(img: HTMLElement | JQuery): mw.Title
+		static isTalkNamespace(namespaceId: namespaceId): boolean
+		static wantSignatureNamespace(namespaceId: namespaceId): boolean
+		static exists(title: title): boolean | null
+		static exist: {
+		    pages: {[title: string]: boolean},
+		    set: (titles: string | string[], state?: boolean) => boolean
+		}
+		static normalizeExtension(extension: string): string
+		static phpCharToUpper(chr: string): string
+
+		getNamespaceId(): namespaceId
+		getNamespacePrefix(): string
+		getName(): string
+		getNameText(): string
+		getExtension(): string | null
+		getDotExtension(): string
+		getMain(): string
+		getMainText(): string
+		getPrefixedDb(): string
+		getPrefixedText(): string
+		getRelativeText(namespace: namespaceId): string
+		getFragment(): string | null
+		getUrl(params: any): string
+		isTalkPage(): boolean
+		getTalkPage(): Title | null
+		getSubjectPage(): Title | null
+		canHaveTalkPage(): boolean
+		exists(): boolean | null
+		toString(): string
+		toText(): string
+	}
+
+	namespace util {
+		const $content: JQuery;
+		function rawurlencode(str: string): string;
+		function escapeIdForAttribute(str: string): string;
+		function escapeIdForLink(str: string): string;
+		function debounce(delay: number, callback: Function): (...args: any[]) => void;
+		function wikiUrlencode(str: string): string;
+		function getUrl(pageName: string, params?: {[param: string]: string}): string;
+		function wikiScript(str: string): string;
+		function addCSS(text: string): any;
+		function getParamValue(param: string, url?: string): string;
+		function hidePortlet(portletId: string): void;
+		function isPortletVisible(portletId: string): boolean;
+		function showPortlet(portletId: string): void;
+		function addPortletLink(portletId: string, href: string, text: string, id?: string,
+			tooltip?: string, accesskey?: string, nextnode?: string): HTMLLIElement;
+		function validateEmail(mailtxt: string): boolean;
+		function isIPv4Address(address: string, allowBlock?: boolean): boolean;
+		function isIPv6Address(address: string, allowBlock?: boolean): boolean;
+		function isIPAddress(address: string, allowBlock?: boolean): boolean;
+		function parseImageUrl(url: string): {
+			name: string;
+			width: number | null;
+			resizeUrl: (w: any) => string;
+		} | null;
+		function escapeRegExp(str: string): string;
+	}
+
+	namespace config {
+
+		// get() and set() are actually on mw.Map() but I guess all that isn't necessary to document
+		// here as we'd never use them
+		function get(configKey: string | string[], fallback?: any): any
+		function set(configKey: string, configValue: any): boolean
+		function exists(configKey: string): boolean
+	}
+
+
+	// Not everything is included
+	namespace loader {
+		/**
+		 * Execute a function after one or more modules are ready.
+		 *
+		 * @param dependencies
+		 * @param {Function} ready Callback to execute when all dependencies are
+		 * ready.
+		 * @param {Function} error Callback to execute if one or more dependencies
+		 * failed.
+		 */
+		function using(dependencies: string[] | string, ready?: Function, error?: Function): JQuery.Promise<any>;
+
+		/**
+		 * Load an external script or one or more modules.
+		 *
+		 * @param {string|Array} modules Either the name of a module, array of modules,
+		 *  or a URL of an external script or style
+		 * @param {string} [type='text/javascript'] MIME type to use if calling with a URL of an
+		 *  external script or style; acceptable values are "text/css" and
+		 *  "text/javascript"; if no type is provided, text/javascript is assumed.
+		 * @throws {Error} If type is invalid
+		 */
+		function load(modules: string | string[], type?: string): () => void;
+		/**
+		 * Get the loading state of the module.
+		 * On of 'registered', 'loaded', 'loading', 'ready', 'error', or 'missing'.
+		 *
+		 * @param module
+		 */
+		function getState(module: string): string | null;
+	}
+
+	/**
+	 * Loads the specified i18n message string.
+	 * Shortcut for `mw.message( key, parameters... ).text()`.
+	 *
+	 * @param messageName i18n message name
+	 */
+	function msg(messageName: string | null): string;
+
+	/**
+	 * Notification
+	 * @param {HTMLElement|HTMLElement[]|jQuery|string} message
+	 * @param {Object} [options] See mw.notification#defaults for the defaults.
+	 * @return {jQuery.Promise}
+	 */
+	function notify(message: string | JQuery | HTMLElement | HTMLElement[],
+		options?: { tag?: string, type?: string, title?: string }): JQuery.Promise<any>;
+
+}
+
---
+++ types/morebits.d.ts
@@ -0,0 +1,287 @@
+/**
+ * Type definitions for morebits.js
+ */
+
+interface FormSubmitEvent extends Event {
+	target: HTMLFormElement
+}
+
+declare namespace Morebits {
+
+	function userIsInGroup(group: string): boolean
+	const userIsSysop: boolean
+
+	function sanitizeIPv6(address: string): string | null
+
+	function isPageRedirect(): boolean
+
+	const pageNameNorm: string
+
+	function pageNameRegex(pageName: string): string
+
+	class quickForm {
+		constructor(event: ((e: FormSubmitEvent) => void), eventType?: string)
+		render(): HTMLFormElement
+		append(quickFormElement): quickFormElement
+		static getInputData(form: HTMLFormElement): Record<string, string>
+		static getElements(form: HTMLFormElement, fieldName: string): HTMLElement[]
+		static getCheckboxOrRadio(elementArray: HTMLInputElement[], value: string): HTMLInputElement
+		static getElementContainer()
+		static getElementLabelObject()
+		static getElementLabel()
+		static setElementLabel()
+		static overrideElementLabel()
+		static resetElementLabel()
+		static setElementVisibility()
+		static setElementTooltipVisibility()
+		static element: typeof quickFormElement
+	}
+
+	namespace string {
+		function toUpperCaseFirstChar(str: string): string
+		function toLowerCaseFirstChar(str: string): string
+		function splitWeightedByKeys(str: string, start: string, end: string, skiplist: string | string[]): string[]
+		function formatReasonText(str: string): string
+		function formatReasonForLog(str: string): string
+		function safeReplace(string: string, pattern: string | RegExp, replacement: string): string
+		function isInfinity(expiry: string): boolean
+		function escapeRegExp(text: string): string
+	}
+
+	namespace array {
+		function uniq<T>(arr: T[]): T[]
+		function dups<T>(arr: T[]): T[]
+		function chunk<T>(arr: T[], size: number): T[][]
+	}
+
+	class unbinder {
+		// has some properties too but they're not supposed to be public
+		unbind(prefix: string, postfix: string): void
+		rebind(): string
+	}
+
+	class date extends Date {
+		private _d: Date
+		isValid(): boolean
+		isBefore(date: Morebits.date | Date): boolean
+		isAfter(date: Morebits.date | Date): boolean
+		getDayName(): string
+		getDayNameAbbrev(): string
+		getUTCDayName(): string
+		getUTCDayNameAbbrev(): string
+		getMonthName(): string
+		getMonthNameAbbrev(): string
+		getUTCMonthName(): string
+		getUTCMonthNameAbbrev(): string
+		add(number: number, unit: string): Morebits.date
+		subtract(number: number, unit: string): Morebits.date
+		format(formatstr: string, zone: number | 'utc' | 'system'): string
+		calendar(zone: number | 'utc' | 'system'): string
+		monthHeaderRegex(): RegExp
+		monthHeader(level?: number): string
+	}
+
+	namespace wiki {
+		let numberOfActionsLeft: number
+		let nbrOfCheckpointsLeft: number
+		let actionCompleted: {
+			(): void
+			event: (() => void)
+			timeout: number
+			redirect: string
+			notice: string
+			followRedirect: boolean
+		}
+		function addCheckpoint(): void
+		function removeCheckpoint(): void
+
+		class api {
+			constructor(currentAction: string, query: any, onSuccess?: ((apiobj: api) => any),
+						statusElement?: string, onFailure?: ((apiobj: api) => any))
+			responseXML: XMLDocument
+			setParent(parent: any): void
+			setStatusElement(statusElement: Morebits.status): void
+			post(callerAjaxParameters?: JQuery.AjaxSettings): JQuery.Promise<api>
+			private returnError(callerAjaxParameters: JQuery.AjaxSettings): JQuery.Promise<api>
+			getStatusElement(): Morebits.status
+			getErrorCode(): string
+			getErrorText(): string
+			getXML(): XMLDocument
+			getResponse(): any
+			static setApiUserAgent(ua: string): void
+			static getToken(): JQuery.Promise<string>
+		}
+
+		class page {
+			constructor(pageName: string, currentAction?: string)
+			load(onSuccess: ((pageobj: page) => void)): void
+			save(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			append(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			prepend(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			newSection(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			getPageName(): string
+			getPageText(): string
+			setPageText(pageText: string)
+			setAppendText(appendText: string)
+			setPrependText(prependText: string)
+			setNewSectionText(newSectionText: string)
+			setNewSectionTitle(newSectionTitle: string)
+			setEditSummary(summary: string): void
+			setChangeTags(tags: string | string[]): void
+			setCreateOption(createOption: string): void
+			setMinorEdit(minorEdit: string): void
+			setBotEdit(botEdit: boolean): void
+			setPageSection(pageSection: string): void
+			setMaxConflictRetries(maxConflictRetries: number): void
+			setMaxRetries(maxRetries: number): void
+			setWatchlist(watchlistOption: string): void
+			setWatchlistExpiry(watchlistExpiry: string): void
+			setWatchlistFromPreferences(watchlistOption: string): void
+			setFollowRedirect(followRedirect, followCrossNsRedirect: string): void
+			setLookupNonRedirectCreator(flag: boolean): void
+			setMoveDestination(destination: string): void
+			setMoveTalkPage(flag: boolean): void
+			setMoveSubpages(flag: boolean): void
+			setMoveSuppressRedirect(flag: boolean): void
+			setEditProtection(level: string, expiry: string): void
+			setMoveProtection(level: string, expiry: string): void
+			setCreateProtection(level: string, expiry: string): void
+			setCascadingProtection(flag: boolean): void
+			suppressProtectWarning(): void
+			setOldID(oldID: string): void
+			getCurrentID(): string
+			getRevisionUser(): string
+			getLastEditTime(): string
+			setCallbackParameters(callbackParameters: any)
+			getCallbackParameters(): any
+			getStatusElement(): Morebits.status
+			setFlaggedRevs(level: string, expiry: string)
+			exists(): boolean
+			getPageID(): string
+			getLoadTime(): string
+			getCreator(): string
+			getCreationTimestamp(): string
+			canEdit(): boolean
+			lookupCreation(onSuccess: ((pageobj: page) => void)): void
+			revert(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			move(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			patrol(): void
+			triage(): void
+			deletePage(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			undeletePage(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			protect(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+			stabilize(onSuccess?: ((pageobj: page) => void), onFailure?: ((pageobj: page) => void)): void
+		}
+
+		class preview {
+			constructor(previewbox: HTMLElement)
+			previewbox: HTMLElement
+			beginRender(wikitext: string, pageTitle: string, sectionTitle: string): void
+			closePreview(): void
+		}
+
+	}
+
+	namespace wikitext {
+		function parseTemplate(text: string, start: number): {name: string, parameters: {[key: string]: string}}
+		class page {
+			text: string
+			removeLink(link_target: string)
+			commentOutImage(image: string, reason: string)
+			addToImageComment(image: string, data: string)
+			removeTemplate(template: string)
+			insertAfterTemplates(tag: string, regex: string | string[], flags: string, preRegex: string | string[])
+			getText(): string
+		}
+	}
+
+	class userspaceLogger {
+		initialText: string
+		headerLevel: number
+		changeTags: string | string[]
+		log(logText: string, summaryText: string): void
+	}
+
+
+	class status {
+		textRaw: string
+		text: DocumentFragment
+		type: 'status' | 'info' | 'warn' | 'error'
+		static init(root: HTMLElement): void
+		static root: HTMLElement
+		onError(handler: ((arg: any) => any)): void // XXX: check handler types
+		link(): void
+		unlink(): void
+		codify(obj: (string | HTMLElement)[])
+		update(status: string, type: 'status' | 'info' | 'warn' | 'error')
+		generate(): void
+		render(): void
+		status(status: string)
+		info(status: string)
+		warn(status: string)
+		error(status: string)
+		static info(text: string, status: string): void
+		static warn(text: string, status: string): void
+		static error(text: string, status: string): void
+		static actionCompleted(text: string): void
+		static printUserText(comments: string, message: string)
+	}
+
+	function htmlNode(type: string, content: string, color?: string): HTMLElement
+	function checkboxShiftClickSupport(jQuerySelector: string | JQuery, jQueryContext: string | JQuery)
+
+	class batchOperation<T> {
+		getStatusElement(): Morebits.status
+		setPageList(pageList: T[]): void
+
+		// Overloaded definition
+		setOption(optionName: any, optionValue: any)
+		setOption(optionName: 'chunkSize', optionValue: number)
+		setOption(optionName: 'preserveIndividualStatusLines', optionValue: boolean)
+
+		run(worker: ((item: T) => any), postFinish: (() => any)): void
+		workerSuccess(arg: any): void
+		workerFailure(arg: any): void
+	}
+
+	class taskManager {
+		taskDependencyMap: Map<Function, Function[]>
+		deferreds: Map<Function, JQuery.Deferred<any>[]>
+		allDeferreds: JQuery.Deferred<any>[]
+		add(func: Function, deps: Function[])
+		execute(): JQuery.Promise<void>
+	}
+
+	class simpleWindow {
+		constructor(width: number, height: number)
+		buttons: Array<any>
+		height: number
+		hasFooterLinks: boolean
+		scriptName: string
+		focus(): Morebits.simpleWindow
+		close(event: Event): Morebits.simpleWindow
+		display(): Morebits.simpleWindow
+		setTitle(title: string): Morebits.simpleWindow
+		setScriptName(name: string): Morebits.simpleWindow
+		setWidth(width: number): Morebits.simpleWindow
+		setHeight(height: number): Morebits.simpleWindow
+		setContent(content: HTMLElement): Morebits.simpleWindow
+		addContent(content: HTMLElement): Morebits.simpleWindow
+		purgeContent(): Morebits.simpleWindow
+		addFooterLink(text: string, wikiPage: string, prep?: boolean): Morebits.simpleWindow
+		setModality(modal: boolean): Morebits.simpleWindow
+		static setButtonsEnabled(enabled: boolean)
+	}
+
+}
+
+// TypeScript's handling of nested classes is pathetic ...
+declare class quickFormElement {
+	constructor(data: any)
+	static id: number
+	append(data: quickFormElement): quickFormElement
+	render(): HTMLElement
+	compute(data: any): any
+	static generateTooltip(node: HTMLElement, data: any): void
+}
+
