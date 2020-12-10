bdcfee33e63984810a7556a07a71bd3b44258217
Amory Meltzer <Amorymeltzer@gmail.com>
2020-12-10 14:59:31 -0500

    tests: Better system for morebits.wikitext.page methods
    Pre-define each test in an array of objects, then pass through each one individually.  Allows testing each method individually, rather than in turn, so it's easier and clearer to define future tests.  Also add a few more specific/complex tests.

--- tests/morebits.wikitext.js
+++ tests/morebits.wikitext.js
@@ -76,17 +76,136 @@ QUnit.test('parseTemplate', assert => {
 	};
 	assert.deepEqual(Morebits.wikitext.parseTemplate(makeTemplate(internal)), internal, 'Internal templates');
 });
+
 QUnit.test('Morebits.wikitext.page', assert => {
 	var text = '{{short description}}{{about}}[[File:Fee.svg]]O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!';
 	var page = new Morebits.wikitext.page(text);
 	assert.true(page instanceof Morebits.wikitext.page, 'Correct instance');
-	assert.strictEqual(page.getText(), text, 'text');
-	assert.strictEqual(page.addToImageComment('Fee.svg', 'thumb|size=42').getText(), '{{short description}}{{about}}[[File:Fee.svg|thumb|size=42]]O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!', 'Add data');
-	assert.strictEqual(page.commentOutImage('Fee.svg', 'too pretty').getText(), '{{short description}}{{about}}<!-- too pretty: [[File:Fee.svg|thumb|size=42]] -->O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!', 'Comment out');
-	assert.strictEqual(page.removeLink('juliet').getText(), '{{short description}}{{about}}<!-- too pretty: [[File:Fee.svg|thumb|size=42]] -->O, she doth {{plural|teach}} the torches to burn bright!', 'Remove link');
-	assert.strictEqual(page.removeTemplate('plural').getText(), '{{short description}}{{about}}<!-- too pretty: [[File:Fee.svg|thumb|size=42]] -->O, she doth  the torches to burn bright!', 'Remove template');
+	assert.strictEqual(page.getText(), text, 'Got text');
 
-	assert.strictEqual(page.insertAfterTemplates('{{newtag}}', 'short description|about').getText(), '{{short description}}{{about}}{{newtag}}<!-- too pretty: [[File:Fee.svg|thumb|size=42]] -->O, she doth  the torches to burn bright!', 'Remove template');
-	assert.throws(() => page.insertAfterTemplates(), 'throws: no tag');
-	assert.throws(() => page.insertAfterTemplates('{{newtag}}'), 'throws: no regex');
+	// Throws
+	assert.throws(() => new Morebits.wikitext.page(text).insertAfterTemplates(), 'throws: no tag');
+	assert.throws(() => new Morebits.wikitext.page(text).insertAfterTemplates('tag'), 'throws: no regex');
+
+	// Define all the tests individually, with the appropriate method,
+	// input, expected output, and (spreaded) parameters.
+	var tests = [
+		{
+			name: 'simple',
+			method: 'removeLink',
+			input: text,
+			expected: '{{short description}}{{about}}[[File:Fee.svg]]O, she doth {{plural|teach}} the torches to burn bright!',
+			params: ['juliet']
+		},
+		{
+			name: 'simple',
+			method: 'commentOutImage',
+			input: text,
+			expected: '{{short description}}{{about}}<!-- too pretty: [[File:Fee.svg]] -->O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			params: ['Fee.svg', 'too pretty']
+		},
+		{
+			name: 'simple',
+			method: 'addToImageComment',
+			input: text,
+			expected: '{{short description}}{{about}}[[File:Fee.svg|thumb|size=42]]O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			params: ['Fee.svg', 'thumb|size=42']
+		},
+		{
+			name: 'simple',
+			method: 'removeTemplate',
+			input: text,
+			expected: '{{short description}}{{about}}[[File:Fee.svg]]O, [[Juliet|she]] doth  the torches to burn bright!',
+			params: ['plural']
+		},
+		{
+			name: 'simple',
+			method: 'insertAfterTemplates',
+			input: text,
+			expected: '{{short description}}{{about}}{{newtag}}[[File:Fee.svg]]O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			params: ['{{newtag}}', 'short description|about']
+		},
+		{
+			name: 'multiple',
+			method: 'removeLink',
+			input: 'O, [[Juliet|she]] [[juliet|doth]] {{plural|teach}} [[Romeo|the]] [[:Juliet|torches]] [[juliet|to]] burn bright!',
+			expected: 'O, she doth {{plural|teach}} [[Romeo|the]] torches to burn bright!',
+			params: ['juliet']
+		},
+		{
+			name: 'multiple',
+			method: 'addToImageComment',
+			input: 'O, [[File:Fee.svg]] she [[File:Fee.svg|doth|teach]] the [[File:Fee.svg|torches]] to burn bright!',
+			expected: 'O, [[File:Fee.svg|thumb|size=42|test]] she [[File:Fee.svg|doth|teach|thumb|size=42|test]] the [[File:Fee.svg|torches|thumb|size=42|test]] to burn bright!',
+			params: ['Fee.svg', ['thumb', 'size=42', 'test'].join('|')]
+		},
+		{
+			name: 'preRegex',
+			method: 'insertAfterTemplates',
+			input: '{{short description}}{{About}}<!-- random -->{{xfd}}O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			expected: '{{short description}}{{About}}<!-- random -->{{xfd}}{{newtag}}O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			params: ['{{newtag}}', 'short description|about|Juliet|xfd', null, ['<!-- random -->', '<!-- comment -->']]
+		},
+		{
+			name: 'Forgot the preRegex',
+			method: 'insertAfterTemplates',
+			input: '{{short description}}{{About}}<!-- random -->{{xfd}}O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			expected: '{{short description}}{{About}}{{newtag}}<!-- random -->{{xfd}}O, [[Juliet|she]] doth {{plural|teach}} the torches to burn bright!',
+			params: ['{{newtag}}', 'short description|about|Juliet|xfd']
+		},
+		{
+			name: 'File links not displays',
+			method: 'removeLink',
+			input: 'O, [[:File:Fee.svg|she]] [[File:Fee.svg|doth]] {{plural|teach}} [[:File:Fee.svg|the]] [[File:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			expected: 'O, she [[File:Fee.svg|doth]] {{plural|teach}} the [[File:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			params: ['File:Fee.svg']
+		},
+		{
+			name: 'File displays not links',
+			method: 'commentOutImage',
+			input: 'O, [[:File:Fee.svg|she]] [[File:Fee.svg|doth]] {{plural|teach}} [[:File:Fee.svg|the]] [[File:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			expected: 'O, [[:File:Fee.svg|she]] <!-- reason: [[File:Fee.svg|doth]] --> {{plural|teach}} [[:File:Fee.svg|the]] <!-- reason: [[File:Fee.svg|torches]] --> [[Fee.svg|to]] burn bright!',
+			params: ['Fee.svg', 'reason']
+		},
+		{
+			name: 'File displays not links',
+			method: 'addToImageComment',
+			input: 'O, [[:File:Fee.svg|she]] [[File:Fee.svg|doth]] {{plural|teach}} [[:File:Fee.svg|the]] [[File:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			expected: 'O, [[:File:Fee.svg|she]] [[File:Fee.svg|doth|size=42]] {{plural|teach}} [[:File:Fee.svg|the]] [[File:Fee.svg|torches|size=42]] [[Fee.svg|to]] burn bright!',
+			params: ['Fee.svg', 'size=42']
+		},
+		{
+			name: 'Image or file',
+			method: 'commentOutImage',
+			input: 'O, [[File:Fee.svg|she]] [[Image:Fee.svg|doth]] {{plural|teach}} [[:File:Fee.svg|the]] [[Image:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			expected: 'O, <!-- [[File:Fee.svg|she]] --> <!-- [[Image:Fee.svg|doth]] --> {{plural|teach}} [[:File:Fee.svg|the]] <!-- [[Image:Fee.svg|torches]] --> [[Fee.svg|to]] burn bright!',
+			params: ['Fee.svg']
+		},
+		{
+			name: 'Image or file',
+			method: 'addToImageComment',
+			input: 'O, [[File:Fee.svg|she]] [[Image:Fee.svg|doth]] {{plural|teach}} [[:File:Fee.svg|the]] [[Image:Fee.svg|torches]] [[Fee.svg|to]] burn bright!',
+			expected: 'O, [[File:Fee.svg|she|size=42]] [[Image:Fee.svg|doth|size=42]] {{plural|teach}} [[:File:Fee.svg|the]] [[Image:Fee.svg|torches|size=42]] [[Fee.svg|to]] burn bright!',
+			params: ['Fee.svg', 'size=42']
+		},
+		{
+			name: 'Alt namespace',
+			method: 'removeTemplate',
+			input: 'O, she doth {{User:ThisIsaTest/plural|teach}} the torches to burn bright!',
+			expected: 'O, she doth  the torches to burn bright!',
+			params: ['User:ThisIsaTest/plural']
+		},
+		{
+			name: 'Template namespace',
+			method: 'removeTemplate',
+			input: 'O, she doth {{Template:plural|teach}} the torches to burn bright!',
+			expected: 'O, she doth  the torches to burn bright!',
+			params: ['Template:plural']
+		}
+	];
+
+	tests.forEach((test) => {
+		var page = new Morebits.wikitext.page(test.input);
+		assert.strictEqual(page[test.method](...test.params).getText(), test.expected, test.method + ' - ' + test.name);
+	});
 });
