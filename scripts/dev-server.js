15db0af6731615c93330d34157b6d321214a8ae5
Siddharth VP <siddharthvp@gmail.com>
2021-06-26 18:52:57 +0530

    Add new dev script
    Should be an improvement over the previous one. Loads the whole of Twinkle as a single file, but requires that the gadget version be disabled to avoid all possibility of testing the wrong version.

---
+++ scripts/dev-server.js
@@ -0,0 +1,77 @@
+/* eslint-env node, es6 */
+
+const http = require('http');
+const fs = require('fs/promises');
+const { mwn } = require('mwn');
+
+async function readFile(filepath) {
+	return (await fs.readFile(__dirname + '/../' + filepath)).toString();
+}
+const server = http.createServer(async (request, response) => {
+	const moduleFiles = (await fs.readdir('./modules')).filter(f => f.endsWith('.js'));
+	const jsFiles = ['morebits.js', 'twinkle.js'].concat(moduleFiles.map(f => 'modules/' + f));
+	const cssFiles = ['morebits.css', 'twinkle.css'];
+
+	let jsCode = `mw.loader.load(['jquery.ui', 'ext.gadget.select2']);`;
+	for (let file of jsFiles) {
+		jsCode += await readFile(file);
+	}
+	for (let file of cssFiles) {
+		let css = (await readFile(file)).replace(/\s+/g, ' ');
+		jsCode += `;mw.loader.addStyleTag('${css}');`;
+	}
+	response.writeHead(200, { 'Content-Type': 'text/javascript; charset=utf-8' });
+	response.end(jsCode, 'utf-8');
+});
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || '5500';
+
+const credentialsProvided = process.env.USERNAME && process.env.PASSWORD;
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Please add "mw.loader.load('http://${hostname}:${port}');" to your on-wiki common.js file to begin testing.` + (!credentialsProvided ? `\nEnsure the Twinkle gadget version is disabled. If you provide your USERNAME and PASSWORD as environment variables, we'll try to automatically disable the gadget for you and re-enable it when you're done testing.` : ''));
+});
+
+const GADGET_NAME = 'Twinkle';
+
+// Disable the deployed gadget version when we begin our testing,
+// enable it back again when we stop testing.
+// You need to create a credentials.json file in this directory
+// (it will be git-ignored) with the "apiUrl", "username" and "password" fields.
+(async () => {
+	if (!credentialsProvided) return;
+	let user;
+	try {
+		user = await mwn.init({
+			"apiUrl": "https://en.wikipedia.org/w/api.php",
+			"username": process.env.USERNAME,
+			"password": process.env.PASSWORD,
+			"silent": true
+		});
+	} catch (e) {
+		if (e instanceof mwn.Error) {
+			console.log(`[mwn]: can't disable twinkle as gadget: login failure: ${e}`);
+			console.log(e.stack);
+		}
+		return;
+	}
+	await user.saveOption('gadget-' + GADGET_NAME, '0');
+	console.log('[i] Disabled twinkle as gadget.');
+
+	// Allow async operations in exit hook
+	process.stdin.resume();
+
+	// Catch ^C
+	process.on('SIGINT', async () => {
+		try {
+			await user.saveOption('gadget-' + GADGET_NAME, '1');
+			console.log('[i] Re-enabled twinkle as gadget.');
+		} catch (e) {
+			console.log(`[i] failed to re-enable twinkle gadget: ${e}`);
+			console.log(e.stack);
+		} finally {
+			process.exit();
+		}
+	});
+})();
