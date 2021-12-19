61226300414673e238c4d8e629fd358c904d791f
NovemLinguae <novemlinguae@gmail.com>
2021-12-19 06:11:05 -0800

    detect {{Talkheader}}

--- modules/friendlywelcome.js
+++ modules/friendlywelcome.js
@@ -630,9 +630,9 @@ Twinkle.welcome.callbacks = {
 		var welcomeText = Twinkle.welcome.getTemplateWikitext(params.type, params.template, params.article);
 
 		if (Twinkle.getPref('topWelcomes')) {
-			var hasTalkHeader = /^\{\{Talk header\}\}/i.test(text);
+			var hasTalkHeader = /^\{\{Talk ?header\}\}/i.test(text);
 			if (hasTalkHeader) {
-				text = text.replace(/^\{\{Talk header\}\}\n{0,2}/i, '');
+				text = text.replace(/^\{\{Talk ?header\}\}\n{0,2}/i, '');
 				text = '{{Talk header}}\n\n' + welcomeText + '\n\n' + text;
 				text = text.trim();
 			} else {
