67b749ebd243d3e3c95fcf6069b340b702a6ce36
NovemLinguae <novemlinguae@gmail.com>
2022-02-24 18:51:57 -0800

    tag: {{Merge to}} combined with an article section has incorrect "Discuss" link

--- modules/friendlytag.js
+++ modules/friendlytag.js
@@ -1527,7 +1527,8 @@ Twinkle.tag.callbacks = {
 								params.talkDiscussionTitleLinked = 'Proposed merge of ' + direction;
 								params.talkDiscussionTitle = params.talkDiscussionTitleLinked.replace(/\[\[(.*?)\]\]/g, '$1');
 							}
-							currentTag += '|discuss=Talk:' + params.discussArticle + '#' + params.talkDiscussionTitle;
+							var articleWithNoSection = params.discussArticle.replace(/^([^#]*)#.*$/, '$1'); // If article name is Test#Section, delete #Section
+							currentTag += '|discuss=Talk:' + articleWithNoSection + '#' + params.talkDiscussionTitle;
 						}
 						break;
 					default:
