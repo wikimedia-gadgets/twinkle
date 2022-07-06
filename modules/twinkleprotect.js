baa8df873b848205932f8c14ea5944e52a9cba51
NovemLinguae <novemlinguae@gmail.com>
2022-07-06 08:05:08 -0700

    safer way to do it (set reason to false)

--- modules/twinkleprotect.js
+++ modules/twinkleprotect.js
@@ -1100,7 +1100,7 @@ Twinkle.protect.callback.evaluate = function twinkleprotectCallbackEvaluate(e) {
 	if (input.actiontype === 'tag' || (input.actiontype === 'protect' && mw.config.get('wgArticleId') && mw.config.get('wgPageContentModel') !== 'Scribunto')) {
 		tagparams = {
 			tag: input.tagtype,
-			reason: (input.tagtype === 'pp-protected' || input.tagtype === 'pp-semi-protected' || input.tagtype === 'pp-move') && input.protectReason,
+			reason: false,
 			small: input.small,
 			noinclude: input.noinclude
 		};
@@ -1443,6 +1443,9 @@ Twinkle.protect.callbacks = {
 			summary = 'Removing protection template';
 		} else {
 			tag = params.tag;
+			if (params.reason) {
+				tag += '|reason=' + params.reason;
+			}
 			if (params.small) {
 				tag += '|small=yes';
 			}
