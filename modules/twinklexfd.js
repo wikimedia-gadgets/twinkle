342adb22cd80be6229d2b3bfa93e88b2d89670af
briick03 <trialpearswiki@gmail.com>
2023-02-24 22:30:38 +0100

    Fix newline issues

--- modules/twinklexfd.js
+++ modules/twinklexfd.js
@@ -2016,8 +2016,8 @@ Twinkle.xfd.callbacks = {
 			var params = pageobj.getCallbackParameters();
 			var statelem = pageobj.getStatusElement();
 
-			var placementRE = /===== ?Requests to revert undiscussed moves ?=====/i;
-			var newtext = text.replace(placementRE, Twinkle.xfd.callbacks.getDiscussionWikitext('rm', params) + '\n$&');
+			var placementRE = /\n\n?(===== ?Requests to revert undiscussed moves ?=====)/i;
+			var newtext = text.replace(placementRE, Twinkle.xfd.callbacks.getDiscussionWikitext('rm', params) + '\n$1');
 			if (text === newtext) {
 				statelem.error('failed to find target spot for the entry');
 				return;
