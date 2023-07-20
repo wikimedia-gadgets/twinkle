f090f1e026195eff319e421fbf8714a1743290fc
NovemLinguae <79697282+NovemLinguae@users.noreply.github.com>
2023-07-19 23:08:07 -0700

    protect: sanitized-css: don't tick <noinclude> check box (#1637)
    * sanitized-css: don't tick <noinclude> check box
    
    * refactor based on code review suggestion
    
    Co-authored-by: Siddharth VP <siddharthvp@gmail.com>
    
    * fix
    
    * Morebits.pageNameNorm.startsWith
    
    * Morebits.pageNameNorm.startsWith
    
    * includes()
    
    ---------
    
    Co-authored-by: Siddharth VP <siddharthvp@gmail.com>

--- modules/twinkleprotect.js
+++ modules/twinkleprotect.js
@@ -517,6 +517,10 @@ Twinkle.protect.callback.changeAction = function twinkleprotectCallbackChangeAct
 				list: Twinkle.protect.protectionTags,
 				event: Twinkle.protect.formevents.tagtype
 			});
+
+			var isTemplateNamespace = mw.config.get('wgNamespaceNumber') === 10;
+			var isAFD = Morebits.pageNameNorm.startsWith('Wikipedia:Articles for deletion/');
+			var isCode = ['javascript', 'css', 'sanitized-css'].includes(mw.config.get('wgPageContentModel'));
 			field1.append({
 				type: 'checkbox',
 				list: [
@@ -530,7 +534,7 @@ Twinkle.protect.callback.changeAction = function twinkleprotectCallbackChangeAct
 						name: 'noinclude',
 						label: 'Wrap protection template with &lt;noinclude&gt;',
 						tooltip: 'Will wrap the protection template in &lt;noinclude&gt; tags, so that it won\'t transclude',
-						checked: mw.config.get('wgNamespaceNumber') === 10 || (mw.config.get('wgNamespaceNumber') === mw.config.get('wgNamespaceIds').project && mw.config.get('wgTitle').indexOf('Articles for deletion/') === 0)
+						checked: (isTemplateNamespace || isAFD) && !isCode
 					}
 				]
 			});
@@ -1079,11 +1083,12 @@ Twinkle.protect.callback.changePreset = function twinkleprotectCallbackChangePre
 
 			// Default settings for adding <noinclude> tags to protection templates
 			var isTemplateEditorProtection = form.category.value === 'pp-template';
-			var isAFD = mw.config.get('wgNamespaceNumber') === mw.config.get('wgNamespaceIds').project && mw.config.get('wgTitle').indexOf('Articles for deletion/') === 0;
+			var isAFD = Morebits.pageNameNorm.startsWith('Wikipedia:Articles for deletion/');
 			var isNotTemplateNamespace = mw.config.get('wgNamespaceNumber') !== 10;
-			if (isTemplateEditorProtection || isAFD) {
+			var isCode = ['javascript', 'css', 'sanitized-css'].includes(mw.config.get('wgPageContentModel'));
+			if ((isTemplateEditorProtection || isAFD) && !isCode) {
 				form.noinclude.checked = true;
-			} else if (isNotTemplateNamespace) {
+			} else if (isCode || isNotTemplateNamespace) {
 				form.noinclude.checked = false;
 			}
 		}
