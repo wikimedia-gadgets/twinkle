383ce92bfbc48a2f9717db9822ad8149b4d074b9
Monik <87751450+Monik2002@users.noreply.github.com>
2023-08-10 15:51:42 +0530

    added {{uw-blar}} and {{uw-articletodraft}} (#1797)
    * first commit
    
    * second commit
    
    * fix 2 code review comments
    
    ---------
    
    Co-authored-by: NovemLinguae <novemlinguae@gmail.com>

--- modules/twinklewarn.js
+++ modules/twinklewarn.js
@@ -943,6 +943,10 @@ Twinkle.warn.messages = {
 			label: 'Bad AIV report',
 			summary: 'Notice: Bad AIV report'
 		},
+		'uw-articletodraft': {
+			label: 'Article moved to draftspace',
+			summary: 'Notice: Article moved to draftspace'
+		},
 		'uw-autobiography': {
 			label: 'Creating autobiographies',
 			summary: 'Notice: Creating autobiographies'
@@ -959,6 +963,10 @@ Twinkle.warn.messages = {
 			label: '"Biting" newcomers',
 			summary: 'Notice: "Biting" newcomers',
 			suppressArticleInSummary: true  // non-standard (user name, not article), and not necessary
+		},
+		'uw-blar': {
+			label: 'Article blanked and redirected',
+			summary: 'Notice: Article blanked and redirected'
 		},
 		'uw-coi': {
 			label: 'Conflict of interest',
