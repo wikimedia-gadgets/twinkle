4a07910e59d1720a4229358f7d5c9edf69ee9131
Matr1x-101 <70490301+Matr1x-101@users.noreply.github.com>
2024-11-06 19:17:45 +0000

    Add dark mode compatibility to Twinkle (#2023)

--- modules/twinkleblock.js
+++ modules/twinkleblock.js
@@ -649,6 +649,7 @@ Twinkle.block.callback.change_action = function twinkleblockCallbackChangeAction
 
 
 		$form.find('[name=pagerestrictions]').select2({
+			theme: 'default select2-morebits',
 			width: '100%',
 			placeholder: 'Select pages to block user from',
 			language: {
@@ -697,6 +698,7 @@ Twinkle.block.callback.change_action = function twinkleblockCallbackChangeAction
 		});
 
 		$form.find('[name=namespacerestrictions]').select2({
+			theme: 'default select2-morebits',
 			width: '100%',
 			matcher: Morebits.select2.matchers.wordBeginning,
 			language: {
