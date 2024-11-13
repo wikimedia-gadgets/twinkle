3ab8bc7f343fc1e45e3c044d5da1815db01896d7
NovemLinguae <novemlinguae@gmail.com>
2024-11-13 06:17:19 -0800

    mention "advert" in the description, so folks searching for "advert" can find it

--- modules/friendlytag.js
+++ modules/friendlytag.js
@@ -722,7 +722,7 @@ Twinkle.tag.article.tagList = {
 			{ tag: 'Essay-like', description: 'written like a personal reflection, personal essay, or argumentative essay' },
 			{ tag: 'Fanpov', description: "written from a fan's point of view" },
 			{ tag: 'Inappropriate person', description: 'uses first-person or second-person inappropiately' },
-			{ tag: 'Promotional', description: 'contains promotional content' },
+			{ tag: 'Promotional', description: 'contains promotional content or is written like an advertisement' },
 			{ tag: 'Resume-like', description: 'written like a resume' },
 			{ tag: 'Manual', description: 'written like a manual or guidebook' },
 			{ tag: 'Cleanup press release', description: 'reads like a press release or news article',
