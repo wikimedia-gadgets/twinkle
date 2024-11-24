44c3ca9e4c8f90180dc89c9a08d1eb686afe5d4a
NovemLinguae <79697282+NovemLinguae@users.noreply.github.com>
2024-11-24 13:29:09 -0800

    rollback: fix "cannot read properties of undefined" (#2071)
    hotfix
    
    https://en.wikipedia.org/wiki/Wikipedia_talk:Twinkle#Problems_with_WP%3ATwinkle

--- twinkle.js
+++ twinkle.js
@@ -189,9 +189,9 @@ Twinkle.getPref = function twinkleGetPref(name) {
 	}
 
 	// Backwards compatibility code because we renamed confirmOnFluff to confirmOnRollback, and confirmOnMobileFluff to confirmOnMobileRollback
-	if (name === 'confirmOnRollback' && Twinkle.prefs.confirmOnFluff !== undefined) {
+	if (name === 'confirmOnRollback' && typeof Twinkle.prefs === 'object' && Twinkle.prefs.confirmOnFluff !== undefined) {
 		return Twinkle.prefs.confirmOnFluff;
-	} else if (name === 'confirmOnMobileRollback' && Twinkle.prefs.confirmOnMobileFluff !== undefined) {
+	} else if (name === 'confirmOnMobileRollback' && typeof Twinkle.prefs === 'object' && Twinkle.prefs.confirmOnMobileFluff !== undefined) {
 		return Twinkle.prefs.confirmOnMobileFluff;
 	}
 
