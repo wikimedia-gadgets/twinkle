de4436f724a73e63371769fbf60f3c40946c643f
Imconfused3456 <confusedscorpion@gmail.com>
2025-02-03 03:51:59 -0500

    Replace with the proper {{Split dab}} instead of the redirect, {{Splitdab}}

--- modules/twinkletag.js
+++ modules/twinkletag.js
@@ -940,7 +940,7 @@ Twinkle.tag.article.tagList = {
 	],
 	Splitting: [
 		{ tag: 'Split', description: 'should be split into multiple pages.', excludeMI: true },
-		{ tag: 'Splitdab', description: 'disambiguation page should be split into multiple pages.', excludeMI: true }
+		{ tag: 'Split dab', description: 'disambiguation page should be split into multiple pages.', excludeMI: true }
 	],
 	Informational: [
 		{ tag: 'GOCEinuse', description: 'currently undergoing a major copy edit by the Guild of Copy Editors', excludeMI: true },
