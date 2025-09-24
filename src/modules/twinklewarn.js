de0058fadd9dd89f42f19e89425d7fbde589c466
IlyasLebleu <quark.the.chaotic.enby@gmail.com>
2025-09-24 11:43:31 +0200

    use let or const instead of var, fix another lint error

--- src/modules/twinklewarn.js
+++ src/modules/twinklewarn.js
@@ -1460,7 +1460,7 @@ Twinkle.warn.callback.change_category = function twinklewarnCallbackChangeCatego
 				label: 'Available templates',
 				extra: {}
 			});
-			
+
 			wrapperOptgroup = wrapperOptgroup.render();
 			container.appendChild(wrapperOptgroup);
 			container = wrapperOptgroup;
@@ -1602,10 +1602,10 @@ Twinkle.warn.callback.change_category = function twinklewarnCallbackChangeCatego
 
 // Checks for substrings, with the relevant safeguards
 const searchMatcher = function (params, data) {
-	var original = data.text.toUpperCase();
-    var term = params.term.toUpperCase();
+	const original = data.text.toUpperCase();
+    const term = params.term.toUpperCase();
 
-    var alias = '';
+    let alias = '';
 	if (typeof data.element.extra !== 'undefined' && typeof data.element.extra.alias !== 'undefined') {
 		alias = data.element.extra.alias.toUpperCase();
 	}
@@ -1631,13 +1631,13 @@ const optgroupFullAlias = function (params, data) {
     if (data.children && data.children.length > 0) {
         // Clone the data object if there are children
         // This is required as we modify the object to remove any non-matches
-        var match = $.extend(true, {}, data);
+        let match = $.extend(true, {}, data);
 
         // Check each child of the option
-		for (var c = data.children.length - 1; c >= 0; c--) {
-			var child = data.children[c];
+		for (let c = data.children.length - 1; c >= 0; c--) {
+			let child = data.children[c];
 
-			var matches = optgroupFullAlias(params, child);
+			let matches = optgroupFullAlias(params, child);
 
 			// If there wasn't a match, remove the object in the array
 			if (matches === null) {
