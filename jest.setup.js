707c6dfff7098c4ae3dd87e97de1addf6eb4dd11
NovemLinguae <novemlinguae@gmail.com>
2025-12-14 01:24:06 -0800

    tests: move jest.setup.js into the root
    Why
    - in MediaWiki repos, test.setup.js is usually located in the root. https://codesearch.wmcloud.org/search/?q=jest%5C.setup%5C.js&files=&excludeFiles=&repos=
    - it is good to copy MediaWiki's style guidelines
    
    What
    - move jest.setup.js from a subfolder to the root directory
    - modify jest.setup.js relative paths
    - modify jest.config.js path to jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -6,15 +6,15 @@ mw.config.set({
 	wgUserGroups: ['interface-admin', 'sysop', '*', 'user', 'autoconfirmed']
 });
 
-require('../src/morebits.js');
-require('../src/twinkle.js');
-require('../src/modules/twinkleblock.js');
-require('../src/modules/twinklespeedy.js');
+require('./src/morebits.js');
+require('./src/twinkle.js');
+require('./src/modules/twinkleblock.js');
+require('./src/modules/twinklespeedy.js');
 // Load this after twinklespeedy.js. Needs to read Twinkle.speedy.data.
-require('../src/modules/twinkleconfig.js');
-require('../src/modules/twinkletag.js');
-require('../src/modules/twinklewarn.js');
-require('../src/modules/twinklexfd.js');
+require('./src/modules/twinkleconfig.js');
+require('./src/modules/twinkletag.js');
+require('./src/modules/twinklewarn.js');
+require('./src/modules/twinklexfd.js');
 global.Morebits = window.Morebits;
 
 global.assert = require('assert');
