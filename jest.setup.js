0bd4d9542ef8d7327716616059b74a0d5a7bbc07
NovemLinguae <79697282+NovemLinguae@users.noreply.github.com>
2025-12-22 05:01:04 -0800

    tests: move jest.setup.js into the root (#2277)
    * tests: move jest.setup.js into the root
    
    Why
    - in MediaWiki repos, test.setup.js is usually located in the root. https://codesearch.wmcloud.org/search/?q=jest%5C.setup%5C.js&files=&excludeFiles=&repos=
    - it is good to copy MediaWiki's style guidelines
    
    What
    - move jest.setup.js from a subfolder to the root directory
    - modify jest.setup.js relative paths
    - modify jest.config.js path to jest.setup.js
    
    * fix linter errors

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint-env node, jest */
 
 // Tweak some mw.configs as needed by tests
 mw.config.set({
@@ -6,18 +7,19 @@ mw.config.set({
 	wgUserGroups: ['interface-admin', 'sysop', '*', 'user', 'autoconfirmed']
 });
 
-require('../src/morebits.js');
-require('../src/twinkle.js');
-require('../src/modules/twinkleblock.js');
-require('../src/modules/twinklespeedy.js');
+require('./src/morebits.js');
+require('./src/twinkle.js');
+require('./src/modules/twinkleblock.js');
+require('./src/modules/twinklespeedy.js');
 // Load this after twinklespeedy.js. Needs to read Twinkle.speedy.data.
-require('../src/modules/twinkleconfig.js');
-require('../src/modules/twinkletag.js');
-require('../src/modules/twinklewarn.js');
-require('../src/modules/twinklexfd.js');
+require('./src/modules/twinkleconfig.js');
+require('./src/modules/twinkletag.js');
+require('./src/modules/twinklewarn.js');
+require('./src/modules/twinklexfd.js');
 global.Morebits = window.Morebits;
 
-global.assert = require('assert');
+const assert = require('assert');
+global.assert = assert;
 
 // Node.js assert doesn't support these functions unlike Qunit assert,
 // so temporarily monkey-patch them
