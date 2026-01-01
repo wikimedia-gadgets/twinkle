69c708b7e5729ab8ccea78f76a9baa33270b5b11
Siddharth VP <siddharthvp@gmail.com>
2026-01-01 22:05:40 +0530

    batchdelete: fix TypeError on submitting with blank page list (#2272)
    Closes #2115

--- src/modules/twinklebatchdelete.js
+++ src/modules/twinklebatchdelete.js
@@ -421,7 +421,7 @@ Twinkle.batchdelete.callback.evaluate = function twinklebatchdeleteCallbackEvalu
 	}
 	Morebits.SimpleWindow.setButtonsEnabled(false);
 	Morebits.Status.init(form);
-	if (input.pages.length === 0) {
+	if (!input.pages || input.pages.length === 0) { // undefined if there are no pages linked from the page
 		Morebits.Status.error('Error', 'nothing to delete, aborting');
 		return;
 	}
