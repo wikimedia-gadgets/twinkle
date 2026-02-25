fe45acb4d704e6691448faf5aabe5c5d1a82d7d5
Siddharth VP <siddharthvp@gmail.com>
2026-02-25 22:30:23 +0530

    Improve caching of Twinkle preferences (#2322)
    Currently, twinkleoptions.js is fetched on every page load. If the options haven't been edited since the last fetch, the response is typically 304 and the data isn't refetched, but the request is still necessary to check if the there has been any change.
    
    Use the API instead of the index.php?action=raw call. The maxage/smaxage parameters are used in the API call so that the response is cached in the browser in a way that avoids revalidation requests, which makes it significantly faster. Expiry of 1 day (86400 seconds) is used for the HTTP cache. Hard reload can be done to clear the cache without waiting for expiry.
    
    Since Twinkle initialization waits for preferences to be available, this also improves Twinkle's load performance.
    
    Closes #2185

--- src/twinkle.js
+++ src/twinkle.js
@@ -303,21 +303,11 @@ Twinkle.addPortletLink = function(task, text, id, tooltip) {
  * **************** General initialization code ****************
  */
 
-const scriptpathbefore = mw.util.wikiScript('index') + '?title=',
-	scriptpathafter = '&action=raw&ctype=text/javascript&happy=yes';
-
 // Retrieve the user's Twinkle preferences
-$.ajax({
-	url: scriptpathbefore + 'User:' + encodeURIComponent(mw.config.get('wgUserName')) + '/twinkleoptions.js' + scriptpathafter,
-	dataType: 'text'
-})
-	.fail(() => {
-		console.log('Could not load your Twinkle preferences, resorting to default preferences'); // eslint-disable-line no-console
-	})
-	.done((optionsText) => {
-
-		// Quick pass if user has no options
-		if (optionsText === '') {
+Morebits.wiki.getCachedPage(`User:${mw.config.get('wgUserName')}/twinkleoptions.js`)
+	.then((optionsText) => {
+		if (!optionsText) {
+			// User has no options
 			return;
 		}
 
@@ -343,6 +333,9 @@ $.ajax({
 		} catch (e) {
 			mw.notify('Could not parse your Twinkle preferences', {type: 'error'});
 		}
+	})
+	.catch(() => {
+		console.log('Could not load your Twinkle preferences, resorting to default preferences'); // eslint-disable-line no-console
 	})
 	.always(() => {
 		$(Twinkle.load);
